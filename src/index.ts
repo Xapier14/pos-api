084293d8e860f4300607e51ad01603d832edeab6
Xapier14 <lancecrisang14@gmail.com>
2024-09-08 19:38:21 +0800

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import { Config } from "./config";
 import multer from "multer";
+import cors from "cors";
 import { BaseRouter } from "./routes";
 
 const app: Express = express();
 const port = Config.Port ?? 3000;
 
 // middlewares
+app.use(cors());
 app.use(multer().any());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
